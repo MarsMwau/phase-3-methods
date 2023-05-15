688173a380394980a94eef66ffb158f492734be7
MarsMwau <marthamumbua16@gmail.com>
2023-05-15 16:47:38 +0300

    Completed Lab

--- methods.rb
+++ methods.rb
@@ -1,1 +1,33 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+# greet("Naureen")
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+# greet_with_default("Sunny")
+
+def add(num1, num2)
+    num1 + num2
+end
+# puts add(1, 2)
+
+def subtract(num1, num2)
+    num1 - num2
+end
+# puts subtract(1, 2)
+
+def halve(number)
+    if number.is_a?(Integer)
+        return number / 2
+    else 
+        return nil
+    end
+end
+#puts halve("no")
